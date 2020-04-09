f78cffa67a1833863616051bee1a92fa82e035a7
Will Brown <brown5628@gmail.com>
2020-04-09 15:22:03 -0400

    revert stg_orders to be 1:1 with source

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,13 +1,7 @@
 select
-    orders.id as order_id,
-    orders.user_id as customer_id,
-    orders.order_date,
-    orders.status,
-    sum(stg_payments.amount) as amount
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
 
 from raw.jaffle_shop.orders
-
-left outer join {{ ref('stg_payments')}}
-    on orders.id = stg_payments.order_id
-
-group by 1,2,3,4
