4d166eedf4d01972a379065c7b6ca8a16558f55c
Will Brown <brown5628@gmail.com>
2020-04-09 11:53:27 -0400

    adjust join in stg_orders to ensure unique ids

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,11 +1,13 @@
 select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status,
-    amount
+    orders.id as order_id,
+    orders.user_id as customer_id,
+    orders.order_date,
+    orders.status,
+    sum(stg_payments.amount) as amount
 
 from raw.jaffle_shop.orders
 
 left outer join {{ ref('stg_payments')}}
     on orders.id = stg_payments.order_id
+
+group by 1,2,3,4
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -2,7 +2,7 @@ select
     ID as payment_id,
     "orderID" as order_id,
     "paymentMethod" as payment_method,
-    AMOUNT as amount,
+    round(AMOUNT/100, 2) as amount,
     CREATED as payment_creation_date
 
 from raw.stripe.payment
